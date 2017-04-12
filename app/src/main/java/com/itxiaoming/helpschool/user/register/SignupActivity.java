0823e1cbd91ccb74df863b2dd69c5881db85b641
jiaming <1149356371@qq.com>
2017-04-12 11:08:21 +0800

    Initial commit

---
+++ app/src/main/java/com/itxiaoming/helpschool/user/register/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.itxiaoming.helpschool.user.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.itxiaoming.helpschool.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
---
+++ app/src/main/java/com/itxiaoming/helpschool/util/AppUtils.java
@@ -0,0 +1,15 @@
+package com.itxiaoming.helpschool.util;
+
+/**
+ * 作者:xjm.
+ * 邮箱:xiaojiaming@infosec.com.cn
+ * 公司:Infosec Technology
+ * 创建时间:Created on 2017/3/2 10:53.
+ * 该类的作用:
+ * 版本号:
+ */
+
+public class AppUtils {
+    //设置TAG的名称
+    private static final String TAG="jiaming";
+}
