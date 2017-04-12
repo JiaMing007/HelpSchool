9a7b6c6adea605e5cbe8d0bcfc9a9bf6dd45ba91
jiaming <1149356371@qq.com>
2017-04-12 11:31:02 +0800

    first commit2

--- app/src/main/java/com/itxiaoming/helpschool/MainActivity.java
+++ app/src/main/java/com/itxiaoming/helpschool/MainActivity.java
@@ -15,26 +15,11 @@ import com.itxiaoming.helpschool.user.login.LoginActivity;
 
 public class MainActivity extends ActionBarActivity {
 
-    private Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnTest = (Button) findViewById(R.id.btn_test);
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "这是用来测试git", Toast.LENGTH_SHORT).show();
-            }
-        });
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "这是用来测试git3", Toast.LENGTH_SHORT).show();
-            }
-        });
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
